e8b1d8033ac5d2ec244b29e740cd1393ec5ea1f7
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-04-30 21:09:26 +0200

    Some extraction and better print

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fmt::Display};
 
 use rss::Channel;
 use scraper::{Html, Selector};
@@ -6,23 +6,46 @@ use scraper::{Html, Selector};
 const _RSS_SEVERE: &str = "https://www.spc.noaa.gov/products/spcwwrss.xml";
 const RSS_ALL: &str = "https://www.spc.noaa.gov/products/spcrss.xml";
 
+struct Warning {
+    title: String,
+    content: String,
+}
+
+impl Display for Warning {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut c = self.content.clone();
+        c.truncate(150);
+        write!(f, "** {} **\n{}\n-------------", self.title, c)
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let channel = get_feed().await?;
-    println!("Loaded channel: {}", channel.title());
+    let warnings = get_warnings(get_feed().await?).await?;
+    for w in warnings {
+        println!("{}", w);
+    }
+    Ok(())
+}
+
+async fn get_warnings(channel: Channel) -> Result<Vec<Warning>, Box<dyn Error>> {
+    let mut warnings = Vec::new();
     for item in channel.items() {
         let title = item.title().unwrap_or("(no title)");
         let desc = item.description().unwrap_or("(nothing)");
-        let d = Html::parse_fragment(desc);
-        let sel = Selector::parse("pre").unwrap();
-        let mut pre = d.select(&sel);
+        let dom = Html::parse_fragment(desc);
+        let sel = Selector::parse("pre")?;
+        let mut pre = dom.select(&sel);
 
-        println!("Item title: {}", title);
         if let Some(n) = pre.next() {
-            println!("{}", n.text().next().unwrap());
+            warnings.push(Warning {
+                title: title.to_string(),
+                content: n.text().next().unwrap().to_string(),
+            })
         }
     }
-    Ok(())
+
+    Ok(warnings)
 }
 
 async fn get_feed() -> Result<Channel, Box<dyn Error>> {
