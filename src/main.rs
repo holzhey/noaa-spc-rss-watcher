b2a67cc607956973868f8e8f25520bc3a54d489e
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-05-27 19:29:52 +0200

    move lib implementation into dedicated repository

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,11 @@
-use std::{error::Error, fmt::Display};
+use std::error::Error;
 
-use serde::Deserialize;
-
-const _RSS_SEVERE: &str = "https://www.spc.noaa.gov/products/spcwwrss.xml";
-const RSS_ALL: &str = "https://www.spc.noaa.gov/products/spcrss.xml";
-
-#[derive(Debug, Deserialize)]
-struct Rss {
-    channel: Channel,
-}
-
-#[derive(Debug, Deserialize)]
-struct Channel {
-    item: Vec<Item>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Item {
-    link: String,
-    title: String,
-    description: String,
-}
-
-struct Warning {
-    title: String,
-    content: String,
-    link: String,
-}
-
-impl Display for Warning {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut c = self.content.clone();
-        c.truncate(150);
-        write!(
-            f,
-            "** {} **\n{}\n{}\n-------------",
-            self.title, self.link, c
-        )
-    }
-}
+use noaa_spc_rss_parser::get_warnings;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let warnings = get_warnings(get_feed()?)?;
+    let warnings = get_warnings()?;
     for w in warnings {
         println!("{}", w);
     }
     Ok(())
-}
-
-fn get_warnings(doc: Rss) -> Result<Vec<Warning>, Box<dyn Error>> {
-    let mut warnings = Vec::new();
-    for item in doc.channel.item {
-        warnings.push(get_warning(item));
-    }
-    Ok(warnings)
-}
-
-fn get_warning(item: Item) -> Warning {
-    let mut desc = "(failed to parse description)";
-
-    let start = item.description.find("<pre>").unwrap_or(0);
-    let finish = item.description.find("</pre>").unwrap_or(desc.len());
-    dbg!(start, finish);
-    if start != 0 && finish != desc.len() {
-        desc = &item.description[start..finish];
-    }
-
-    Warning {
-        title: item.title.to_string(),
-        content: desc.to_string(),
-        link: item.link,
-    }
-}
-
-fn get_feed() -> Result<Rss, Box<dyn Error>> {
-    let content = reqwest::blocking::get(RSS_ALL)?.text()?;
-    let rss: Rss = quick_xml::de::from_str(content.as_str()).unwrap();
-    Ok(rss)
 }
